a160d127a31a5995b17f04dc828552d6f4ae5201
Rishaba Priyan <rishabapriyan@gmail.com>
2022-01-18 20:51:45 +0530

    added mock backend using json server

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,20 +1,23 @@
-import {useContext} from 'react'
+import { useContext } from 'react'
 import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackStats() {
-    const { feedback } = useContext(FeedbackContext)
-    let calculateAverage = 
-            feedback.reduce((accumulator,current) => 
-                {return accumulator + current.rating}
-            ,0) /feedback.length
-    calculateAverage = calculateAverage.toFixed(1).replace(/[.,]0$/, '')
+  const { feedback } = useContext(FeedbackContext)
 
-    return (
-        <div className='feedback-stats'> 
-            <h4>{feedback.length} Reviews</h4>
-            <h4>Average rating: {isNaN(calculateAverage)?0:calculateAverage}</h4>
-        </div>
-    )
+  // Calculate ratings avg
+  let average =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating
+    }, 0) / feedback.length
+
+  average = average.toFixed(1).replace(/[.,]0$/, '')
+
+  return (
+    <div className='feedback-stats'>
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  )
 }
 
 export default FeedbackStats
---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,12 @@
+import spinnerGif from '../assets/spinner.gif'
+function Spinner() {
+    return (
+        <img 
+            src={spinnerGif} 
+            alt='Loading...' 
+            style={{width:'100px',margin:'auto',display:'block'}}
+        />
+    )
+}
+
+export default Spinner
